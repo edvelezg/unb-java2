484f3554d0f329f519c55274ec6f22b69a0e8476
Ed <edvelez.g@gmail.com>
2016-10-29 16:29:46 -0500

    various chgs to my study materials

--- src/other/Permutations.java
+++ src/other/Permutations.java
@@ -1,43 +1,45 @@
 package other;
 public class Permutations {
-	private boolean[] used;
-	private StringBuilder out = new StringBuilder();
-	private final String in;
+    private boolean[] used;
+    private StringBuilder out = new StringBuilder();
+    private final String in;
 
-	public static void main(String[] args) {
-		Permutations p = new Permutations("abcd");
-		p.permute();
-	}
+    public static void main(String[] args) {
+        Permutations p = new Permutations("abc");
+        p.permute();
+    }
 
-	public Permutations(final String str) {
-		in = str;
-		used = new boolean[in.length()];
-	}
+    public Permutations(final String str) {
+        in = str; 
+        used = new boolean[in.length()];
+    }
 
-	public void permute() {
-		if (out.length() == in.length()) {
-			System.out.println(out);
-			return;
-		}
-		for (int i = 0; i < in.length(); ++i) {
-			if (used[i])
-				continue;
-			out.append(in.charAt(i));
-			used[i] = true;
-			permute();
-			used[i] = false;
-			out.setLength(out.length() - 1);
-			System.out.println(String.format("%5d%8s%12s", i, out, join(used)));		
-		}
-	}
+    public void permute() {
+        System.out.println("permute()");
+        if ( out.length() == in.length() ) {
+            System.out.println("out.length() == in.length()");
+            System.out.println(out);
+            return;
+        }
+        for ( int i = 0; i < in.length(); ++i ) {
+            if ( used[i] ) continue;
+            out.append(in.charAt(i));
+            used[i] = true;
+            System.out.println(String.format("befor: %5d%8s%12s", i, out, join(used)));
+            permute();
+            used[i] = false;
+            out.setLength(out.length() - 1);
+            System.out.println(String.format("after: %5d%8s%12s", i, out, join(used)));
+        }
+    }
 
-	public String join(boolean[] array) {
-		StringBuffer b = new StringBuffer();
-		for (int i = 0; i < array.length - 1; i++) {
-			b.append(array[i] ? "T": "F");
-			b.append(",");
-		}
-		b.append(array[array.length - 1] ? "T" : "F");
-		return b.toString();
-	}
+    public String join(boolean[] array) {
+        StringBuffer b = new StringBuffer();
+        for ( int i = 0; i < array.length - 1; i++ ) {
+            b.append(array[i] ? "T" : "F");
+            b.append(",");
+        }
+        b.append(array[array.length - 1] ? "T" : "F");
+        return b.toString();
+    }
 }
