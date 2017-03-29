73de50d4e9f9a469e908ba0b1e58049246119ec1
EdGV <egutarra@tibco.com>
2017-03-28 20:15:24 -0500

    My own creation Diagrams.pptx, and my HasPathsClass enhancement solution

--- src/google/interview/HasPathsClass.java
+++ src/google/interview/HasPathsClass.java
@@ -1,56 +1,54 @@
 package google.interview;
 
 public class HasPathsClass {
-	
+
 	private static int[][] mtrx = {
-			{ 0, 1, 0, 0 },
-			{ 1, 1, 0, 1 },
-			{ 1, 0, 0, 1 },
-			{ 1, 1, 1, 1 },
-			{ 1, 0, 0, 0 }
+			{ 0, 1, 0, 0, 0 },
+			{ 1, 1, 0, 1, 0 },
+			{ 1, 0, 1, 1, 1 },
+			{ 1, 1, 1, 0, 1 },
+			{ 0, 0, 0, 0, 1 }
 			};
 
 	private static int[][] visited = {
-            { 0, 0, 0, 0 },
-            { 0, 0, 0, 0 },
-            { 0, 0, 0, 0 },
-            { 0, 0, 0, 0 }
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 }
             };
-	
-	public static boolean hasPath(int[][] mtx, int row, int numrows, int col, int numcols)
-	{
-		System.out.println("hasPath(r="+row+", c="+ col +", n="+numcols+")");
-		if (row > numrows-1 || col > numcols-1) return false; 
-		
-		if (mtx[row][col] == 0) return false;
-		
-		if (row == numrows-1) return (mtrx[row][col] == 1);
-		
-		if (visited[row][col] == 1) return false;
-		
-		boolean hasP = (mtx[row][col] == 1);
-		visited[row][col] = 1;
-		return hasP && (hasPath(mtx, row, numrows, col+1, numcols) || hasPath(mtx, row+1, numrows, col, numcols) || hasPath(mtx, row, numrows, col-1, numcols));
-	}
-	
 
-	public static void main(String[] args) {
-		
-		int[][] mtx = HasPathsClass.mtrx;
-		int row = 0;
-		boolean hasP = false;
-//		for (int c = 0; c < mtx[0].length; c++) {
-//			hasP = hasPath(mtx, row, mtx.length, c, mtx[0].length);
-//			System.out.println(hasP);
-//			if (hasP) {
-////				break;
-//			}
-//		}
-		
-        hasP = hasPath(mtx, row, mtx.length, 1, mtx[0].length);
-        System.out.println(hasP);
+    public static boolean hasPath(int[][] mtx, int row, int numrows, int col, int numcols) {
+        System.out.println("hasPath(r=" + row + ", c=" + col + ", n=" + numcols + ")");
+        if (row > numrows - 1 || col > numcols - 1)
+            return false;
 
-	}
+        if (mtx[row][col] == 0)
+            return false;
 
+        if (row == numrows - 1)
+            return (mtrx[row][col] == 1);
 
+        if (visited[row][col] == 1)
+            return false;
+
+        boolean hasP = (mtx[row][col] == 1);
+        visited[row][col] = 1;
+        return hasP && (hasPath(mtx, row, numrows, col + 1, numcols) || hasPath(mtx, row + 1, numrows, col, numcols)
+                || hasPath(mtx, row, numrows, col - 1, numcols) || hasPath(mtx, row - 1 , numrows, col, numcols));
+    }
+
+    public static void main(String[] args) {
+
+        int[][] mtx = HasPathsClass.mtrx;
+        int row = 0;
+        boolean hasP = false;
+        for (int c = 0; c < mtx[0].length; c++) {
+            hasP = hasPath(mtx, row, mtx.length, c, mtx[0].length);
+            System.out.println(hasP);
+            if (hasP) {
+//                break;
+            }
+        }
+    }
 }
