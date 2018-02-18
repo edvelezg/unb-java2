2469df58e10a571c5b872c57b3a226455144eeb8
Ed <edvelez.g@gmail.com>
2018-02-17 21:10:27 -0600

    added SortAMap

---
+++ src/amazon/interview/SortAMap.java
@@ -0,0 +1,46 @@
+package amazon.interview;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class SortAMap {
+	public static void main(String[] args) {
+		
+		Map<String, Integer> unsortedMap = new HashMap<>();
+		unsortedMap.put("apple", 30);
+		unsortedMap.put("pear", 50);
+		unsortedMap.put("orange", 20);
+		unsortedMap.put("berry", 60);
+		
+		Set<Entry<String, Integer>> entrySet = unsortedMap.entrySet();
+		LinkedList<Entry<String, Integer>> entryList = new LinkedList<Entry<String, Integer>>(entrySet);
+		Collections.sort(entryList, new Comparator<Entry<String, Integer>>() {
+			@Override
+			public int compare(Entry<String, Integer> arg0, Entry<String, Integer> arg1) {
+				return arg1.getValue().compareTo(arg0.getValue());
+			}
+		});
+		
+		System.out.println(entryList);
+		
+		LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+		for (Entry<String, Integer> entry : entryList) {
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+		
+		System.out.println(sortedMap);
+		
+//		Collections.sort((List<T>) unsortedMap);
+//		Entry<String, Integer> entry = new Entry<String, Integer>("apple", 20);
+//		list.add(  );
+				
+	}
+}
