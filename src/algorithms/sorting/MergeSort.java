b1732b7f806f85d34d191d5de17766cade8418a9
EdGV <egutarra@tibco.com>
2018-03-07 12:05:51 -0600

    changed MergeSort

--- src/algorithms/sorting/MergeSort.java
+++ src/algorithms/sorting/MergeSort.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class MergeSort {
 
     public static void main(String[] args) {
-        int[] array = {5, 3, 2, 0, 1, 7};
-        mergeSort(array, array.length);
+        int[] array = {8, 7, 2, 3, 9, 4, 1, 5};
+        split(array, array.length);
 
         System.out.println(Arrays.toString(array));
 //        return result.length() > 0 ? result.substring(0, result.length() - 1): "";
     }
     
-    public static void mergeSort(int[] a, int n) {
+    public static void split(int[] a, int n) {
         System.out.println(String.format("mergeSort(a=%s n=%d)", Arrays.toString(a), n));
         
         if (n == 1) {
@@ -22,8 +22,8 @@ public class MergeSort {
         int[] left = Arrays.copyOfRange(a, 0, n/2);
         int[] right = Arrays.copyOfRange(a, n/2, n);
         
-        mergeSort(left, left.length);
-        mergeSort(right, right.length);
+        split(left, left.length);
+        split(right, right.length);
 
         merge(a, left, right);
     }
