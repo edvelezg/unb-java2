1a12520f8a4132d0b92ca12f25dc6e6e5154a059
Ed <edvelez.g@gmail.com>
2019-03-18 19:00:37 -0500

    Improvements to Quicksort2

--- src/algorithms/sorting/Quicksort2.java
+++ src/algorithms/sorting/Quicksort2.java
@@ -4,16 +4,17 @@ import java.util.Arrays;
 
 public class Quicksort2 {
 	private static int count = 0;
-	
-	public static void qs(int[] a) {
-		qs(a, 0, a.length-1);
-	}
-	
-	public static int part(int[] a, int l, int r) {
-		int[] ca = Arrays.copyOfRange(a, l, r+1);
-		System.out.println(Arrays.toString(ca));
 
-		int p = a[(l+r)/2];
+	public static void qs(int[] a) {
+		qs(a, 0, a.length - 1);
+	}
+
+	public static int part(int[] a, int l, int r) {
+
+		int p = a[(l + r) / 2];
+		int[] ca = Arrays.copyOfRange(a, l, r + 1);
+		System.out.println(Arrays.toString(ca) + " " + "p: " + p);
+
 		while (l < r) {
 			while (a[l] < p) {
 				l++;
@@ -23,15 +24,13 @@ public class Quicksort2 {
 			}
 			if (l <= r) {
 				swap(a, l, r);
-//				System.out.println(String.format("l%d, r%d, p%d", l, r, p));
 				l++;
 				r--;
 			}
 		}
-//		System.out.println(l);
 		return l;
 	}
-	
+
 	private static void swap(int[] a, int l, int r) {
 		int t = a[l];
 		a[l] = a[r];
@@ -39,25 +38,37 @@ public class Quicksort2 {
 	}
 
 	public static void qs(int[] a, int l, int r) {
-		
-		System.out.println(String.format("%d, %d", l, r));
-		if (l >= r || count > 90) {
-//			int[] ca = Arrays.copyOfRange(a, l, r+1);
-//			System.out.println(Arrays.toString(ca));
-			
+
+		if (l >= r) {
+			int[] ca = Arrays.copyOfRange(a, l, l + 1);
+			// System.out.println(Arrays.toString(ca));
 			return;
 		}
-		count+=1;
-		
+
 		int idx = part(a, l, r);
-		qs(a, l, idx-1);
+		printLft(a, l, idx);
+		qs(a, l, idx - 1);
+		printRgt(a, r, idx);
 		qs(a, idx, r);
 	}
-	
+
+	private static void printLft(int[] a, int l, int idx) {
+		// System.out.print(String.format("qs(%s", Arrays.toString(a)));
+		System.out.print(String.format("qs(a"));
+		System.out.println(String.format(", %d, %d) | %s ", l, idx - 1, Arrays.toString(Arrays.copyOfRange(a, l, idx))));
+	}
+
+	private static void printRgt(int[] a, int r, int idx) {
+		// System.out.print(String.format("qs(%s", Arrays.toString(a)));
+		System.out.print(String.format("qs(a"));
+		System.out.println(String.format(", %d, %d) | %s", idx, r, Arrays.toString(Arrays.copyOfRange(a, idx, r + 1))));
+	}
+
+
 	public static void main(String[] args) {
-        int[] array = new int[] { 5, 1, 4, 7, 9, 2, 8, 3, 6};
-        qs(array);
-        System.out.println(Arrays.toString(array));
+		int[] array = new int[] { 5, 1, 4, 7, 9, 2, 8, 3, 6 };
+		qs(array);
+		System.out.println(Arrays.toString(array));
 	}
 
 }
